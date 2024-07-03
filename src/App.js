d34715e71ee9282af31f64360d5ca0ccb9f95665
vikrant <vikrantgeed@gmail.com>
2024-07-03 16:00:02 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
 
         <Routes>
-          <Route path="/" element={<MainContent />} />
+          <Route path="/webearl-website" element={<MainContent />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<AboutSupplier />} />
         </Routes>
